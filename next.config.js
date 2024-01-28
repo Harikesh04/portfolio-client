208c82655a8c12a9fdf73f66041847218b5c1813
Harry <harikeshkrsingh20@gmail.com>
2024-01-28 15:25:48 +0530

    reverted change

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-import path from 'path';
-
-require('dotenv').config({ path: path.resolve(process.cwd(),'.vercel/env.production.local') });
+const nextConfig = {}
 
 module.exports = nextConfig
